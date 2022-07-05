32a5c5364f279d32f85ba01fcf21df2f0be55f60
Seungjin <infinissible@gmail.com>
2022-07-04 21:42:40 -0700

    heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+  '/',
   './index.html',
   './manifest.json',
   './css/styles.css',
@@ -22,7 +23,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache: ' + CACHE_NAME);
+      // console.log('installing cache: ' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -43,7 +44,7 @@ self.addEventListener('activate', function (e) {
       return Promise.all(
         keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cach : ' + keyList[i]);
+            // console.log('deleting cach : ' + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
@@ -52,56 +53,18 @@ self.addEventListener('activate', function (e) {
   );
 });
 
-// // Respond with cached resources
-// self.addEventListener('fetch', function (e) {
-//   if (e.request.url.includes('/api')) {
-//     e.respondWith(
-//       caches
-//         .open(CACHE_NAME)
-//         .then((cache) => {
-//           return fetch(e.request)
-//             .then((response) => {
-//               // If the response was good, clone it and store it in the cache
-//               if (response.status === 200) {
-//                 cache.put(e.rquest.url, response.clone());
-//               }
-//               return response;
-//             })
-//             .catch((err) => {
-//               // Network request failed, try to get it from the cache
-//               return cache.match(e.request);
-//             });
-//         })
-//         .catch((err) => console.log(err))
-//     );
-//   }
-
-//   e.respondWith(
-//     fetch(e.request).catch(function () {
-//       return caches.match(e.request).then(function (response) {
-//         if (response) {
-//           return response;
-//         } else if (e.request.headers.get('accept').includes('text/html')) {
-//           // return the cached home page for all requests for html pages
-//           return caches.match('/index.html');
-//         }
-//       });
-//     })
-//   );
-// });
-
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url);
+  // console.log('fetch request : ' + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
         // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url);
+        // console.log('responding with cache : ' + e.request.url);
         return request;
       } else {
         // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url);
+        // console.log('file is not cached, fetching : ' + e.request.url);
         return fetch(e.request);
       }
 
